51ce0f30a424c45f3bd9cb31561dba42304c6dd8
shjn0 <nguyentung.pdu@gmail.com>
2018-05-04 17:05:03 +0700

    2nd commit

--- Gemfile
+++ Gemfile
@@ -9,6 +9,12 @@ end
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails'
 gem 'bootstrap-sass'
+gem 'bcrypt'
+gem 'faker'
+gem 'carrierwave'
+gem 'mini_magick'
+gem 'will_paginate'
+gem 'bootstrap-will_paginate'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 # Use Puma as the app server
@@ -49,6 +55,10 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+end
+
+group :production do
+  gem 'fog'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,25 @@
 Rails.application.routes.draw do
-  root                'static_pages#home'
-  get '/help',    to: 'static_pages#help'
-  get '/about',   to: 'static_pages#about'
-  get '/contact', to: 'static_pages#contact'
+  get    'users/new'
 
-  get                 'sessions/new'
-  post                'sessions/create'
-  delete              'sessions/destroy'
+	root   'static_pages#home'
+	get    '/help',    to: 'static_pages#help'
+	get    '/about',   to: 'static_pages#about'
+	get    '/contact', to: 'static_pages#contact'
 
-  resources :users
+	get    '/signup',  to: 'users#new'
+	post   '/signup',  to: 'users#create'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
+
+	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
