4331f08551dfee201f8e2559a1a1759d583d0e0b
shjn0 <nguyentung.pdu@gmail.com>
2018-05-09 16:23:56 +0700

    6th commit

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,5 +3,16 @@ class CommentsController < ApplicationController
   end
 
   def create
+    @comment = current_user.comments.create
+    if @comment.save
+      flash[:success] = "Comment created!"
+      redirect_back_or user
+    else
+      render 'microposts/show'
+    end
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content, :micropost_id, :user_id, :parent_id)
   end
 end
